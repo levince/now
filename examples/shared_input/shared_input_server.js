313afd00aea6ca2776214596d3124787ce4283c6
Vincent HETRU <levince@gmail.com>
2011-04-04 22:37:06 +0200

    adding another example of a realtime shared input

---
+++ examples/shared_input/shared_input_server.js
@@ -0,0 +1,27 @@
+﻿var fs = require('fs');
+var server = require('http').createServer(function(req, response){
+  fs.readFile(__dirname+'/shared_input.html', function(err, data){
+    response.writeHead(200, {'Content-Type':'text/html'});
+    response.write(data);
+    response.end();
+  });
+});
+server.listen(8080);
+var everyone = require("now").initialize(server);
+
+
+everyone.connected(function(){
+  // This may show up as undefined in IE, Firefox, Safari, because alert boxes are not blocking
+  console.log("Joined: " + this.now.name);
+});
+
+
+everyone.disconnected(function(){
+  console.log("Left: " + this.now.name);
+});
+
+
+everyone.now.inputUpdate = function(message)
+{
+	everyone.now.propagateUpdate(this.now.name, message);
+};
